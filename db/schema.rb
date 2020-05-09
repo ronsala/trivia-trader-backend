9b0127d9485cbb4df4513111159062de077d6eef
RonSala <ron.sala@gmail.com>
2020-05-09 16:02:09 -0400

    create question model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_09_190722) do
+ActiveRecord::Schema.define(version: 2020_05_09_200033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,18 @@ ActiveRecord::Schema.define(version: 2020_05_09_190722) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id"
     t.integer "user_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.integer "game_id"
+    t.string "q"
+    t.string "aa"
+    t.string "ab"
+    t.string "ac"
+    t.string "ad"
+    t.string "correct"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
