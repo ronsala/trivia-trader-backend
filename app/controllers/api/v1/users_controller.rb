8638d1c416e07b07c55e62599b414927f6b6d2f5
RonSala <ron.sala@gmail.com>
2020-05-11 20:40:40 -0400

    change users_controller variables to instance, add attributes to user_serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,16 @@
 class Api::V1::UsersController < ApplicationController
 
   def index
-    users = User.all
-    render json: users
+    @users = User.all
+    render json: UserSerializer.new(@users)
   end
 
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
     if user.save
-      render json: user, status: :accepted
+      render json: @user, status: :accepted
     else
-      render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+      render json: {errors: @user.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
@@ -23,7 +23,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def delete
-    
+
   end
 
   private
