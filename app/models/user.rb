ea4a27d65375de63c8efab10b7327c43c9cc7c58
RonSala <ron.sala@gmail.com>
2020-06-13 12:02:46 -0400

    add User #self.from_token_payload

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :games, dependent: :destroy
   has_secure_password
   validates :username, :email, presence: true, uniqueness: true
+
+  def self.from_token_payload(payload)
+    self.find payload[:sub]
+  end
 end
