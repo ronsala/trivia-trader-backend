dc7e18c14bd8a63c9a42a3e4144d0c28d2cb3aad
RonSala <ron.sala@gmail.com>
2020-06-13 12:31:30 -0400

    namespace user_token_controller and route under api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
   namespace :api do
     namespace :v1 do
       resources :users, only: %i[index create update delete]
+      post 'user_token' => 'user_token#create'
     end
   end
 end
