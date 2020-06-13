32beac630183118999bb9cab95f1f3890a2e8d1b
RonSala <ron.sala@gmail.com>
2020-06-13 15:50:11 -0400

    set up back end for Categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::API
-  include Knock::Authenticable
-  before_action :authenticate_user
-  before_action :set_default_format
+  # TODO
+  # include Knock::Authenticable
+  # before_action :authenticate_user
+  # before_action :set_default_format
 
-  private
+  # private
 
-  def set_default_format
-    request.format = :json
-  end
+  # def set_default_format
+  #   request.format = :json
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: %i[index create update delete]
+      resources :categories, only: %i[index]
       post 'user_token' => 'user_token#create'
       post 'find_user' => 'users#find'
     end
