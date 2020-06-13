1a90eb4e1cbe02923639e4df61faa8d72faeef38
RonSala <ron.sala@gmail.com>
2020-06-13 12:34:46 -0400

    add password_digest validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :games, dependent: :destroy
   has_secure_password
   validates :username, :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true, length: { minimum: 6 }
 
   def self.from_token_payload(payload)
     self.find payload[:sub]
