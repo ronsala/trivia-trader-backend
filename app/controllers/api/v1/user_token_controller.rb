df1e91fede883375dcc264f5999a038e55edfdf2
RonSala <ron.sala@gmail.com>
2020-06-13 13:54:04 -0400

    debug knock setup

--- app/controllers/api/v1/user_token_controller.rb
+++ app/controllers/api/v1/user_token_controller.rb
@@ -1,2 +1,8 @@
-class UserTokenController < Knock::AuthTokenController
+class Api::V1::UserTokenController < Knock::AuthTokenController
+  # TODO
+  # #Trying this. Didn't see it in docs.
+  # def create
+  #   super
+  # end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
+  before_action :authenticate_user
+  before_action :set_default_format
+
+  private
+
+  def set_default_format
+    request.format = :json
+  end
 end
