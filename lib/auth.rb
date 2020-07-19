8a52d5c9d7a868747dce7067c26f8789a7e30879
RonSala <ron.sala@gmail.com>
2020-07-19 09:44:50 -0400

    set up lib/auth.rb

--- lib/auth.rb
+++ lib/auth.rb
@@ -1,14 +1,16 @@
-require 'jwt'
+require 'JWT'
 
 class Auth
 
   def self.create_token(user_object)
-    byebug
-    # payload = { user: }
+    payload = { user: JSON.parse(user_object.to_json) }
+    # TODO: Set secret and algo to env variables.
+    token = JWT.encode(payload, 'sekret', 'HS256')
+    decode_token(token)
   end
 
-  def self.decode_token
-
+  def self.decode_token(token)
+    decoded = JWT.decode(token, 'sekret', true, { algorithm: 'HS256' })
   end
   
 end
