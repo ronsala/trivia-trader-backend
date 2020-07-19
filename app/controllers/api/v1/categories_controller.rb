eda076714ff1fba5424c8eb1bbda19c524303fb8
RonSala <ron.sala@gmail.com>
2020-07-19 14:32:11 -0400

    temporarily disable auth

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -4,12 +4,13 @@ class Api::V1::CategoriesController < ApplicationController
   # before_action :authenticate_user
 
   def index
-    token = request.env["HTTP_AUTHORIZATION"]
-    if token && Auth.decode_token(token)
+    # TODO: Restore auth after JWT works.
+    # token = request.env["HTTP_AUTHORIZATION"]
+    # if token && Auth.decode_token(token)
       @categories = Category.all
       render json: CategorySerializer.new(@categories)
-    else
-      render json: { error: { message: "You must have a valid token!"}}, status: 500
-    end
+    # else
+    #   render json: { error: { message: "You must have a valid token!"}}, status: 500
+    # end
   end
 end
