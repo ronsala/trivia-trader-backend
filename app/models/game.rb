029ea33c97a2ae8299348c7d6d5b87ae5fc376fd
RonSala <ron.sala@gmail.com>
2020-08-15 13:31:54 -0400

    add validations to Game

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,6 @@ class Game < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_many :questions, dependent: :destroy
+  validates :title, presence: true, uniqueness: true
+  validates :category_id, :user_id, presence: true
 end
